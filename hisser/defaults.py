fca3a3500dfdf493f3b9ba0fa82655dbb6ee50a2
Anton Bobrov <baverman@gmail.com>
2018-08-11 15:05:40 +0300

    change default retention

--- hisser/defaults.py
+++ hisser/defaults.py
@@ -8,7 +8,7 @@ DATA_DIR = None
 
 # Default retentions, resolution:keep-interval,...
 # Greater resolution must be an integer factor of a smaller one.
-RETENTIONS = '1m:3d, 5m:15d, 30m:90d, 6h:3y'
+RETENTIONS = '1m:3d, 5m:15d, 30m:90d, 2h:1y'
 
 # Default aggregations, pattern|method,...
 # Patterns are applied in sorted order.
