327351b009a1e8303d12aaceb484e89705f496ec
Anton Bobrov <baverman@gmail.com>
2018-08-12 23:36:52 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ setup(
     entry_points={
         'console_scripts': ['hisser = hisser.cli:cli']
     },
-    classifiers=(
+    classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-    )
+    ]
 )
