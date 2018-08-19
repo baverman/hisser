cfddb49c2bcb1d46bcedbb0c4e1d3c6e16a41334
Anton Bobrov <baverman@gmail.com>
2018-08-19 22:17:41 +0300

    release 0.10

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 name=${1:-baverman/graphite-hisser}
 graphite_version=${2:-1.1.3}
-hisser_version=${3:-0.9}
+hisser_version=${3:-0.10}
 tag=${4:-$graphite_version-$hisser_version-1}
 
 cd $(dirname $0)
